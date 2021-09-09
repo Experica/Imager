9d37dad24cf7c5cf2c03f8f7d3b5ad3be3a9d7a7
Li Alex Zhang <fff008@gmail.com>
2021-09-08 22:50:52 -0700

    Update GenerateColorMap.jl

--- GenerateColorMap.jl
+++ GenerateColorMap.jl
@@ -1,9 +1,9 @@
 ## ColorMap in Config for Imager
-using Colors,YAML
+using Colors,ColorSchemes,YAML
 
 
 cs = [RGB(HSL(360*i/255,1,0.5)) for i in 0:255]
+cs = ColorSchemes.turbo.colors
+
 cm = Dict("ColorMap"=>map(c->round.(Int,255*[c.r,c.g,c.b]),cs))
-
-
 YAML.write_file(joinpath(@__DIR__,"colormap.yaml"),cm)
