58156ca4f276f736faa0e888002335ddd065642b
Li Alex Zhang <fff008@gmail.com>
2023-11-21 17:48:59 -0800

    update packages

--- ImagerCommandDotnet/generated/SliceCompile.ImagerCommand.d
+++ ImagerCommandDotnet/generated/SliceCompile.ImagerCommand.d
@@ -2,8 +2,8 @@
 <dependencies>
   <source name="..\ImagerCommand.ice">
     <options>
-      <IceHome>C:\Users\SNLC_2P\Imager\packages\zeroc.ice.net.3.7.9\build\..</IceHome>
-      <IceToolsPath>C:\Users\SNLC_2P\Imager\packages\zeroc.ice.net.3.7.9\build\..\tools</IceToolsPath>
+      <IceHome>C:\Users\SNLC_2P\Imager\packages\zeroc.ice.net.3.7.10\build\..</IceHome>
+      <IceToolsPath>C:\Users\SNLC_2P\Imager\packages\zeroc.ice.net.3.7.10\build\..\tools</IceToolsPath>
       <OutputDir>C:\Users\SNLC_2P\Imager\ImagerCommandDotnet\generated</OutputDir>
     </options>
   </source>
